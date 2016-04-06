1c9e5afd6e2ad0031bdd56f34817e1f1a7e1c784
George Dhoore <georgiepoo@gmail.com>
2016-04-06 18:14:57 -0400

    Random goat authors

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,55 @@
-$(function() {
-  $('#tweetQuote').on('click', function(e){
-    e.preventDefault();
-    // Full attribution
-    // window.open('https://twitter.com/intent/tweet?hashtags=QuotesMcGoats' +
-    //             '&text=' + encodeURIComponent($('#quoteText').text()) +
-    //             ' -' + encodeURIComponent($('#goatAuthor').text()) +
-    //             ' (commonly misattributed to ' +
-    //             encodeURIComponent($('#realAuthor').text()) + ')', '_blank');
+(function() {
+  // onReady
+  $(function() {
 
-    // Just fake author
-    window.open('https://twitter.com/intent/tweet?hashtags=QuotesMcGoats' +
-                '&text=' + encodeURIComponent($('#quoteText').text()) +
-                ' -' + encodeURIComponent($('#goatAuthor').text()), '_blank');
-  });
-});
+    $('#tweetQuote').on('click', function(e){
+      e.preventDefault();
+      // Full attribution - Little long for twitter
+      // window.open('https://twitter.com/intent/tweet?hashtags=QuotesMcGoats' +
+      //             '&text=' + encodeURIComponent($('#quoteText').text()) +
+      //             ' -' + encodeURIComponent($('#goatAuthor').text()) +
+      //             ' (commonly misattributed to ' +
+      //             encodeURIComponent($('#realAuthor').text()) + ')', '_blank');
+
+      // Just fake author
+      window.open('https://twitter.com/intent/tweet?hashtags=QuotesMcGoats' +
+                  '&text=' + encodeURIComponent($('#quoteText').text()) +
+                  ' -' + encodeURIComponent($('#goatAuthor').text()), '_blank');
+    });  // tweetQuote
+
+    $('#newQuote').on('click', function(e) {
+      e.preventDefault();
+      $('#goatAuthor').text(goat.getRandomName());
+    }); // newQuote
+
+    // Get our first quote
+    $('#newQuote').click();
+  });  // onReady
+
+  // Name ideas:
+  // Goat, sheep, kid, billy, ewe, lamb, chop, ibex, markhor,
+  // capra, ram, aries, wool, fleece, bleat, baaa, dolly, blacksheep,
+  // horny/horn
+  var goat = {
+    names: [
+      "Goaty McGoatface",     "Little Bo Peep's Lost Sheep", "Mary's Lamb",
+      "Goat John Paul II",    "Lao Ewe",                     "Billy The Kid",
+      "Ewe Boll",             "Billy Idol",                  "Billy Gates",
+      "Sun Ewe",              "J. K. Ramling",               "Rami",
+      "Vincent Van Goat",     "Muhammad Dolly",              "Wooly Allen",
+      "Ali Baaaaba",          "Ramstein",                    "Abralamb Lincoln",
+      "Alexander the Goat",   "Tupac Markhor",               "Ram Rather",
+      "Daniel Ramcliffe",     "Ram Weasley",                 "Ewegene Levy",
+      "Ewegene Mirman",       "Ewegene Porter",              "Billy Joel",
+      "BILLY MAYS",           "Billy Crystal",               "Billy Zane",
+      "Ram Swanson",          "Ram Burgundy",                "Aries Spears",
+      "Kid Meier",            "Kid Vicious",                 "Kid Wilson",
+      "Kid Barrett",          "River Ram",                   "Simon Ram",
+      "Reuben Ram"
+    ],
+    getRandomName: function() {
+      return this.names[Math.floor(Math.random() * this.names.length)];
+    }
+  };
+
+}());
